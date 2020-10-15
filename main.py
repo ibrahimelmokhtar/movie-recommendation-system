09be909c71d76d8286ce6aadf26a3622c7de58e1
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2020-10-16 01:53:16 +0200

    Add try/except for wrong movie title

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from functions import *
+import sys
 
 
 def main():
     # enter movie title: (for example)
-    movieTitle = "inception"
+    movieTitle = "iron man"
 
     # get movie's rating:
     source = 'Rotten Tomatoes'
-    movieRating, movieID = get_movie_ratingAndID(movieTitle, source)
+    try:
+        movieRating, movieID = get_movie_ratingAndID(movieTitle, source)
+    except:
+        print("Movie title is NOT correct")
+        sys.exit(0)
 
     print(f"Title: {movieTitle}")
     print(f"\tIMDB ID: {movieID}")
